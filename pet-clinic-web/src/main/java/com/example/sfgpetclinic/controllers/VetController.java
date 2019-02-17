5c5294252636fb9fb171b900c34651d0544e6fb2
misiek <krzmis@gmail.com>
2019-02-17 18:59:09 +0000

    Added Vets page & Controller.

---
+++ pet-clinic-web/src/main/java/com/example/sfgpetclinic/controllers/VetController.java
@@ -0,0 +1,14 @@
+package com.example.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"vets/index", "/vets", "vets/index.html"})
+    public String vets() {
+        return "vets/index";
+    }
+
+}

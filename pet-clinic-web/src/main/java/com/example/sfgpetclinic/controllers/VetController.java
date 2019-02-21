48f51a2df9c3f3bcf8da08cfce6dbea594812c9d
misiek <krzmis@gmail.com>
2019-02-21 21:44:34 +0000

    Added Vet Controller and table for vets page

--- pet-clinic-web/src/main/java/com/example/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/example/sfgpetclinic/controllers/VetController.java
@@ -1,13 +1,22 @@
 package com.example.sfgpetclinic.controllers;
 
+import com.example.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
-    @RequestMapping({"vets/index", "/vets", "vets/index.html"})
-    public String vets() {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping()
+    public String vets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 
